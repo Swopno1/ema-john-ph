af9f1b425deb072298b08117e0ef331935d00b48
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-24 11:50:12 +0600

    Added Pagination, changes hooks, etc.

--- client/src/components/Shop/Shop.js
+++ client/src/components/Shop/Shop.js
@@ -7,8 +7,31 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-  const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [page, size]);
+
+  // return [products, setProducts];
+
+  useEffect(() => {
+    fetch('http://localhost:5000/productCount')
+      .then((res) => res.json())
+      .then((data) => {
+        const count = data.count;
+        const pages = Math.ceil(count / 10);
+
+        setPageCount(pages);
+      });
+  }, []);
 
   useEffect(() => {
     const storedCart = getStoredCart();
@@ -52,6 +75,23 @@ const Shop = () => {
             handleAddToCart={handleAddToCart}
           ></Product>
         ))}
+        <div className='pagination '>
+          {[...Array(pageCount).keys()].map((number) => (
+            <button
+              key={number}
+              className={page === number ? 'selected' : ''}
+              onClick={() => setPage(number)}
+            >
+              {number}
+            </button>
+          ))}
+          <select onChange={(e) => setSize(e.target.value)}>
+            <option value='5'>5</option>
+            <option value='10'>10</option>
+            <option value='15'>15</option>
+            <option value='20'>20</option>
+          </select>
+        </div>
       </div>
       <div className='cart-container'>
         <Cart cart={cart}>
--- server/index.js
+++ server/index.js
@@ -22,11 +22,28 @@ async function run() {
     const productCollection = client.db('ama-john-ph').collection('product');
 
     app.get('/product', async (req, res) => {
+      console.log(req.query);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
       const query = {};
       const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
+      let products;
+      if (page || size) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
 
       res.send(products);
+    });
+
+    app.get('/productCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+
+      res.send({ count });
     });
   } finally {
   }
