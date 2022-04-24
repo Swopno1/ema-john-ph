61fecea6f88b15e7b8927e052faf590d02a9370a
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-24 09:34:57 +0600

    Setup server with mongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiekl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db('ama-john-ph').collection('product');
+
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 // Root
 app.get('/', (req, res) => {
